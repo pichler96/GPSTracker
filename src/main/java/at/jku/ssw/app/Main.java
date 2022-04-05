7133edf2d89016736790c8a2c9d6788a4fbedb23
Thomas Pichler <pichler.thomas96@gmail.com>
2022-04-05 15:25:48 +0200

    loadData Function

--- src/main/java/at/jku/ssw/app/Main.java
+++ src/main/java/at/jku/ssw/app/Main.java
@@ -5,10 +5,18 @@ import at.jku.ssw.tcxparser.schema.ActivityT;
 
 import javax.xml.bind.JAXBException;
 import java.awt.*;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import at.jku.ssw.tcxparser.schema.TrainingCenterDatabaseT;
+import org.apache.commons.io.FilenameUtils;
+
 
 public class Main {
 
@@ -16,42 +24,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
-
-
         try {
-            TcxParser parser = new TcxParser();
-            List<ActivityT> activities = new ArrayList<ActivityT>();
-            activities.add(parser.parseTCX(new FileInputStream("data/2020/barcelona.tcx")).getActivities().getActivity().get(0));
-            activities.add(parser.parseTCX(new FileInputStream("data/2021/wolfgangsee.tcx")).getActivities().getActivity().get(0));
-            activities.add(parser.parseTCX(new FileInputStream("data/2021/tucson.tcx")).getActivities().getActivity().get(0));
-            activities.add(parser.parseTCX(new FileInputStream("data/2021/sabino.tcx")).getActivities().getActivity().get(0));
+            for (TrainingCenterDatabaseT training : loadData()) {
+                for (ActivityT activity : training.getActivities().getActivity()) {
+                    System.out.println(activity.getCreator().getName() + " start " + activity.getSport());
+                    activity.getLap().forEach(a -> System.out.println("LAP start: " + a.getStartTime() + " for TotalTime " + a.getTotalTimeSeconds() + " Distance: " +a.getDistanceMeters() + " Speed: " + a.getMaximumSpeed()));
+                    System.out.println();
+                }
 
-            list= activities;
-            for (ActivityT activity : activities) {
-                System.out.println(activity.getCreator().getName() + " start " + activity.getSport());
-                activity.getLap().forEach(a -> System.out.println("LAP start: " + a.getStartTime() + " for TotalTime " + a.getTotalTimeSeconds() + " Distance: " +a.getDistanceMeters() + " Speed: " + a.getMaximumSpeed()));
-                System.out.println();
             }
 
         } catch (JAXBException | FileNotFoundException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        EventQueue.invokeLater(new Runnable() {
-
-            @Override
-            public void run(){
-                SwingMain m= new SwingMain();
-                m.setVisible(true);
-            }
-        });
-
+        // In SwingMain
+        //EventQueue.invokeLater(new Runnable() {
+        //    @Override
+        //    public void run(){
+        //        SwingMain m= new SwingMain();
+        //        m.setVisible(true);
+        //   }
+        //});
     }
 
     public static List<ActivityT> getListOfTracks(){
         return list;
     }
 
+    public static List<TrainingCenterDatabaseT> loadData() throws JAXBException, IOException {
+        TcxParser parser = new TcxParser();
+        List<TrainingCenterDatabaseT> trainings = new ArrayList<>();
+
+        //Parase all files in data
+        File directoryPath = new File("data");
+
+        //List of all files and directories
+        for(File training : directoryPath.listFiles()) {
+            if (FilenameUtils.getExtension(training.getName()).equals("tcx")) {
+                trainings.add(parser.parseTCX(new FileInputStream(training.getPath())));
+            }
+        }
+        return trainings;
+    }
 }
